1e44e4a4e61a8ab8ebf3c0527bda8a161441a31e
home sw <csw999944@naver.com>
2023-08-28 09:41:09 +0900

    23.08.28 홈(메인페이지) 태그 만드는중

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -3,23 +3,31 @@ const NavbarContainer = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
-  padding: 20px 40px;
+  padding: 50px 200px;
 `;
 const Logo = styled.div`
   font-size: 30px;
   font-weight: bold;
+  /* margin-right: -100px; */
 `;
 
 const MenuContainer = styled.div`
   margin-right: 50px;
+  display: flex;
 `;
-const MenuItem = styled.div``;
+const MenuItem = styled.a`
+  margin-right: 70px;
+`;
 
 const ButtonContainer = styled.div`
   width: 20%;
   display: flex;
+  /* justify-content: space-between; */
 `;
-const SignInButton = styled.div``;
+const SignInButton = styled.div`
+  margin-right: 30px;
+  background-color: ;
+`;
 const SignUpButton = styled.div``;
 
 export default function NavBar() {
@@ -27,12 +35,11 @@ export default function NavBar() {
     <NavbarContainer>
       <Logo>GL-CO</Logo>
       <MenuContainer>
-        <MenuItem></MenuItem>
+        <MenuItem href="/home">홈</MenuItem>
+        <MenuItem href="/home">소개</MenuItem>
+        <MenuItem href="/home">서비스</MenuItem>
+        <MenuItem href="/home">문의</MenuItem>
       </MenuContainer>
-      <a href="#home">홈</a>
-      <a href="#about">소개</a>
-      <a href="#service">서비스</a>
-      <a href="#contact">문의</a>
       <ButtonContainer>
         <SignInButton>Sign In</SignInButton>
         <SignUpButton>Sign Up</SignUpButton>
--- src/Routes/Home.js
+++ src/Routes/Home.js
@@ -1,4 +1,29 @@
 import NavBar from "../Components/NavBar";
+import styled from "styled-components";
+const bgImage = styled.div`
+  width : 50%
+  height:400px
+`;
+
 export default function Home() {
-  return <NavBar></NavBar>;
+  return (
+    <div>
+      <NavBar></NavBar>
+      <div className="배경이미지">
+        <h2>Title</h2>
+        <p>ff</p>
+      </div>
+      <div className="부가기능라인">
+        <div className="태그창"></div>
+        <div className="검색창"></div>
+        <div className="검색아이콘"></div>
+      </div>
+      <div className="밑에네모박스">
+        <div className="작은박스">
+          <div className="프로필"></div>
+          <div className="텍스트"></div>
+        </div>
+      </div>
+    </div>
+  );
 }
