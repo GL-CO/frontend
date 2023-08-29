ae9e0fe34cdfa28cd9a8aa64170d8ccd72c51287
home sw <csw999944@naver.com>
2023-08-29 10:05:13 +0900

    23.08.29 state 변수명, event handler 함수명 변경. api 테스트를 위한 서버 /api/users/login생성해야함.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './components/LoginPage/Login';
-import Join from './components/LoginPage/Join';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./components/LoginPage/Login";
+import Join from "./components/LoginPage/Join";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/join" element={<Join />} />
-      </Routes>
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/join" element={<Join />} />
+        </Routes>
+      </Router>
+    </QueryClientProvider>
   );
 }
 
 export default App;
-
--- src/components/LoginPage/Join.js
+++ src/components/LoginPage/Join.js
@@ -1,74 +1,124 @@
-import React, {useState, useEffect} from 'react';
-import styled from 'styled-components';
-import { Link } from 'react-router-dom';
-
-
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import { useQuery } from "react-query";
+import dummyData from "../../dummyData.json";
+import { useMutation } from "react-query";
+async function signUpUser({ username }) {
+  const res = await fetch("/api/users/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ username }),
+  });
+  if (!res.ok) {
+    const errorData = await res.json();
+    throw new Error(errorData.error);
+  }
+  const data = await res.json();
+  return data;
+}
+async function testSignUpUser() {
+  const userData = { username: "user0" };
+  try {
+    const res = await signUpUser(userData);
+    console.log("로그인성공", res);
+  } catch (error) {
+    console.error("로그인실패", error.message);
+  }
+}
 const Join = () => {
-    const [Email, setEmail] = useState("");
-    const [Password, setPassword] = useState("");
-    const [ConfirmPassword, setConfirmPassword] = useState("");
-    const [Name, setName] = useState("");
-    const [NickName, setNickName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [username, setUsername] = useState("");
+  const [nickname, setNickname] = useState("");
 
-    const onNameHandler = (event) => { 
-        setName(event.currentTarget.value);
+  const handleEmailChange = (event) => {
+    setEmail(event.currentTarget.value);
+  };
+  const handlePasswordChange = (event) => {
+    setPassword(event.currentTarget.value);
+  };
+  const handleConfirmPasswordChange = (event) => {
+    setConfirmPassword(event.currentTarget.value);
+  };
+  const handleUsernameChange = (event) => {
+    setUsername(event.currentTarget.value);
+  };
+  const handleNicknameChange = (event) => {
+    setNickname(event.currentTarget.value);
+  };
+  const handleError = () => {
+    if (password !== confirmPassword) {
+      alert("비밀번호가 같지 않습니다.");
+      return false;
     }
-    const onEmailHandler = (event) => { 
-        setEmail(event.currentTarget.value);
-    }
-    const onPasswordHandler = (event) => { 
-        setPassword(event.currentTarget.value);
-    }
-    const onConfirmPasswordHandler = (event) => { 
-        setConfirmPassword(event.currentTarget.value);
-    }
-
-    const onNickNameHandler = (event) => { 
-        setNickName(event.currentTarget.value);
-    }
-
-    const errorHandler = () => {
-        if (Password !== ConfirmPassword) {
-          alert("비밀번호가 같지 않습니다.");
-          return false;
-        }
     return true;
-    };
+  };
 
+  const mutation = useMutation(signUpUser);
+  const handleSignUp = (event) => {
+    event.preventDefault();
+    mutation.mutate({ username });
+    console.log(mutation);
+  };
+  testSignUpUser();
   return (
     <JoinWrapper>
       <Container>
-        
         <SignIncontainer>
           <h1> 환영합니다! </h1>
-          <p> 로그인 하러 가기  </p>
+          <p> 로그인 하러 가기 </p>
           <LoginLink href="/Login"> Sign Up </LoginLink>
         </SignIncontainer>
 
         <FormContainer>
-          <form action="#">
+          <form action="/join">
             <h1>Sign Up</h1>
-            <Label> 
-              <Input type="email" placeholder = "Email" onChange={onEmailHandler} />
-            </Label>
-            
             <Label>
-              <Input type="password" placeholder="Password" onChange={onPasswordHandler} />
+              <Input
+                type="email"
+                placeholder="email"
+                onChange={handleEmailChange}
+              />
             </Label>
 
-            <Label> 
-              <Input type="password" placeholder="ConfirmPassword" onChange={onConfirmPasswordHandler} />
+            <Label>
+              <Input
+                type="password"
+                placeholder="password"
+                onChange={handlePasswordChange}
+              />
             </Label>
 
-            <Label> 
-              <Input type="Name" placeholder="Name" onChange={onNameHandler} />
+            <Label>
+              <Input
+                type="password"
+                placeholder="confirmPassword"
+                onChange={handleConfirmPasswordChange}
+              />
             </Label>
 
-            <Label> 
-              <Input type="Name" placeholder="NickName" onChange={onNickNameHandler} />
+            <Label>
+              <Input
+                type="username"
+                placeholder="username"
+                onChange={handleUsernameChange}
+              />
             </Label>
-           
-            <SubmitButton>Sign Up</SubmitButton>
+
+            <Label>
+              <Input
+                type="username"
+                placeholder="nickname"
+                onChange={handleNicknameChange}
+              />
+            </Label>
+
+            <SubmitButton onClick={handleSignUp}>Sign Up</SubmitButton>
           </form>
         </FormContainer>
       </Container>
@@ -83,15 +133,15 @@ const JoinWrapper = styled.div`
   justify-content: center;
   align-items: center;
   height: 100vh;
-  background: #fff; 
-  color: #000; 
+  background: #fff;
+  color: #000;
   overflow: hidden;
 `;
 
 const Container = styled.div`
   display: flex;
   justify-content: center;
-  width: 100%; 
+  width: 100%;
 `;
 
 const SignIncontainer = styled.div`
@@ -100,29 +150,26 @@ const SignIncontainer = styled.div`
   align-items: center;
   justify-content: center;
   padding: 40px;
-  background: #8F4646;
+  background: #8f4646;
   width: 50%;
   text-align: center;
- 
 
   h1 {
     font-size: 24px;
     margin-bottom: 20px;
     color: #fff;
   }
-  
-  p{
-    color: #fff;
-    margin-bottom: 20px; 
-  }
 
+  p {
+    color: #fff;
+    margin-bottom: 20px;
+  }
 `;
 
 const FormContainer = styled.div`
-  background: #FFF0F0;
+  background: #fff0f0;
   padding: 20px;
   width: 50%;
-
 
   h1 {
     font-size: 24px;
@@ -130,8 +177,8 @@ const FormContainer = styled.div`
     text-align: center;
   }
 
-  a{ 
-    text-align: center; 
+  a {
+    text-align: center;
   }
 
   form {
@@ -151,20 +198,20 @@ const FormContainer = styled.div`
   }
 `;
 const Label = styled.label`
-  display: block; 
-  text-align: center; 
-`
-const Input  = styled.input`
+  display: block;
+  text-align: center;
+`;
+const Input = styled.input`
   border: none;
   border-bottom: 2px solid #800000; /* 밑줄 */
   padding: 10px;
   font-size: 16px;
   background: transparent; /* 입력란 배경을 투명하게 설정 */
-  margin-bottom: 20px; 
+  margin-bottom: 20px;
 `;
 
 const SubmitButton = styled.button`
-  background: #800000; 
+  background: #800000;
   color: #fff;
   padding: 10px;
   border: none;
@@ -174,21 +221,21 @@ const SubmitButton = styled.button`
   transition: background 0.3s;
 
   &:hover {
-    background: #945050; 
+    background: #945050;
   }
 `;
 
 const LoginLink = styled.a`
-background: transparent;
-border: 2px solid #fff;
-color: #fff;
-padding: 10px 20px;
-cursor: pointer;
-transition: background 0.3s, color 0.3s;
-font-size: 16px;
+  background: transparent;
+  border: 2px solid #fff;
+  color: #fff;
+  padding: 10px 20px;
+  cursor: pointer;
+  transition: background 0.3s, color 0.3s;
+  font-size: 16px;
 
-&:hover {
-  background: #fff;
-  color: #ff416c;
-}
+  &:hover {
+    background: #fff;
+    color: #ff416c;
+  }
 `;
