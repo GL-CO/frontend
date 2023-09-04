abd6ebae2b94755ce0ae477f3349a941b6c73ef8
me <me@naver.com>
2023-09-04 15:28:45 +0900

    app.js 수정정

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Login from "./pages/LoginPage/Login";
 import Join from "./pages/LoginPage/Join";
-import { QueryClient, QueryClientProvider } from "react-query";
-
-const queryClient = new QueryClient();
 
 function App() {
   return (
     <BrowserRouter>
       {/* <Header /> */}
       <Routes>
-        <Route path="/login" element={<LoginPage />}></Route>
-        <Route path="/write" element={<Write />}></Route>
-        <Route path="/" element={<Home />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/join" element={<Join />} />
       </Routes>
     </BrowserRouter>
   );
