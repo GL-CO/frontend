11540ee973aeb6f684516cb96a38565f647e7075
chasyuss <cotn963@gmail.com>
2023-09-27 13:09:49 +0900

    feat: add Mypage

--- src/pages/LoginPage/Login.js
+++ src/pages/LoginPage/Login.js
@@ -1,91 +1,37 @@
-import React, { useState}from 'react';
-import styled from 'styled-components';
-import axios from 'axios';
+import React from "react";
+import styled from "styled-components";
+import NavBar from "../../Components/NavBar";
 
 const Login = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [showEmailError, setShowEmailError] = useState(false);
-  const [showPasswordError, setShowPasswordError] = useState(false);
-
-
-
-  const handleLogin = async (e) => {
-    e.preventDefault();
-
-
-    if(!email) {
-      setShowEmailError(true);
-      return;
-    }
-
-    if(!password) {
-      setShowPasswordError(true);
-      return;
-    }
-
-    try {
-      const response = await axios.post('http://ec2-43-201-96-213.ap-northeast-2.compute.amazonaws.com:8080/v1/user/login', {
-        email,
-        password,
-      });
-
-      if (response.status === 200) {
-        console.log('로그인 성공', response.data);
-       
-      } else {
-        console.error('로그인 실패', response.data);
-       
-      }
-    } catch (error) {
-      console.error('로그인 실패', error);
-    }
-  };
-  
   return (
-    <LoginWrapper>
-      <Container>
-        
-        <SignUpcontainer>
-          <h1>반갑습니다!</h1>
-          <p> 회원가입 하러 가기 </p>
-          <JoinLink href="/Join"> Sign Up </JoinLink>
-        </SignUpcontainer>
+    <div>
+      <NavBar></NavBar>
+      <LoginWrapper>
+        <Container>
+          <SignUpcontainer>
+            <h1>반갑습니다!</h1>
+            <p> 회원가입 하러 가기 </p>
+            <JoinLink href="/Join"> Sign In </JoinLink>
+          </SignUpcontainer>
 
-        <FormContainer>
-          <form onSubmit={handleLogin}>
-            <h1>Sign In</h1>
-            <Label> 
-              <Input type="email" 
-              placeholder="Email"
-              value={email}
-              onChange={(e) => {
-                setEmail(e.target.value);
-                setShowEmailError(false);
-              }} 
-              />
-            </Label>
-        
-            
-            <Label>
-              <Input type="password" 
-              placeholder="Password" 
-              value={password}
-              onChange={(e) => {
-                setPassword(e.target.value);
-                setShowPasswordError(false);
-              }}
-              />
-            </Label>
-           
-           
-            <a href="#">Forgot your password?</a>
-            <SubmitButton type="submit">Sign in</SubmitButton>
-          </form>
-        </FormContainer>
+          <FormContainer>
+            <form action="#">
+              <h1>Sign In</h1>
+              <Label>
+                <Input type="email" placeholder="Email" />
+              </Label>
 
-      </Container>
-    </LoginWrapper>
+              <Label>
+                <Input type="password" placeholder="Password" />
+              </Label>
+
+              <a href="#">Forgot your password?</a>
+              <SubmitButton>Sign in</SubmitButton>
+            </form>
+          </FormContainer>
+        </Container>
+      </LoginWrapper>
+    </div>
   );
 };
 
@@ -96,15 +42,15 @@ const LoginWrapper = styled.div`
   justify-content: center;
   align-items: center;
   height: 100vh;
-  background: #fff; 
-  color: #000; 
+  background: #fff;
+  color: #000;
   overflow: hidden;
 `;
 
 const Container = styled.div`
   display: flex;
   justify-content: center;
-  width: 100%; 
+  width: 100%;
 `;
 
 const SignUpcontainer = styled.div`
@@ -112,29 +58,26 @@ const SignUpcontainer = styled.div`
   flex-direction: column;
   align-items: center;
   padding: 40px;
-  background: #8F4646;
+  background: #8f4646;
   width: 50%;
   text-align: center;
- 
 
   h1 {
     font-size: 24px;
     margin-bottom: 20px;
     color: #fff;
   }
-  
-  p{
+
+  p {
     color: #fff;
-    margin-bottom: 20px; 
+    margin-bottom: 20px;
   }
 `;
 
-
 const FormContainer = styled.div`
-  background: #FFF0F0;
+  background: #fff0f0;
   padding: 20px;
   width: 50%;
-
 
   h1 {
     font-size: 24px;
@@ -142,8 +85,8 @@ const FormContainer = styled.div`
     text-align: center;
   }
 
-  a{ 
-    text-align: center; 
+  a {
+    text-align: center;
   }
 
   form {
@@ -163,20 +106,20 @@ const FormContainer = styled.div`
   }
 `;
 const Label = styled.label`
-  display: block; 
-  text-align: center; 
-`
-const Input  = styled.input`
+  display: block;
+  text-align: center;
+`;
+const Input = styled.input`
   border: none;
   border-bottom: 2px solid #800000; /* 밑줄 추가 */
   padding: 10px;
   font-size: 16px;
   background: transparent; /* 입력란 배경을 투명하게 설정 */
-  margin-bottom: 20px; 
+  margin-bottom: 20px;
 `;
 
 const SubmitButton = styled.button`
-  background: #800000; 
+  background: #800000;
   color: #fff;
   padding: 10px;
   border: none;
@@ -186,21 +129,21 @@ const SubmitButton = styled.button`
   transition: background 0.3s;
 
   &:hover {
-    background: #945050; 
+    background: #945050;
   }
 `;
 
 const JoinLink = styled.a`
-background: transparent;
-border: 2px solid #fff;
-color: #fff;
-padding: 10px 20px;
-cursor: pointer;
-transition: background 0.3s, color 0.3s;
-font-size: 16px;
+  background: transparent;
+  border: 2px solid #fff;
+  color: #fff;
+  padding: 10px 20px;
+  cursor: pointer;
+  transition: background 0.3s, color 0.3s;
+  font-size: 16px;
 
-&:hover {
-  background: #fff;
-  color: #ff416c;
-}
+  &:hover {
+    background: #fff;
+    color: #ff416c;
+  }
 `;
