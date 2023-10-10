9990e5c0fd4d23ab53167681f9c6786f88811089
chasyuss <cotn963@gmail.com>
2023-10-10 15:36:12 +0900

    feat: add mywrite page

--- src/pages/Stories.js
+++ src/pages/Stories.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
-import React from "react";
 import NavBar from "../Components/NavBar";
 const Container = styled.div`
   display: flex;
